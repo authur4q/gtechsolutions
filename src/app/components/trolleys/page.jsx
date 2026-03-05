fccb650ccf746dea3a9786c848c9a57b1ae62fc3
authur4q <authurbass@gmail.com>
2026-03-05 22:26:52 +0300

    Change fetch URL to relative path for Trolleys

--- src/app/components/trolleys/page.jsx
+++ src/app/components/trolleys/page.jsx
@@ -10,7 +10,7 @@ function Trolleys() {
       useEffect(() => {
         const productsData = async () => {
           try {
-            const res = await fetch("http://localhost:3000/api/category?category=Trolleys");
+            const res = await fetch("/api/category?category=Trolleys");
             const data = await res.json();
             const formattedData = Array.isArray(data) ? data : (data.products || []);
             setProducts(formattedData);
