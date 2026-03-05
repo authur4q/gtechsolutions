bdfe71abfd50eb525985db181725564a3320acbf
authur4q <authurbass@gmail.com>
2026-03-05 22:20:34 +0300

    Update API endpoint for fetching fridge categories

--- src/app/components/fridges/page.jsx
+++ src/app/components/fridges/page.jsx
@@ -10,7 +10,7 @@ function Fridges() {
       useEffect(() => {
         const productsData = async () => {
           try {
-            const res = await fetch("http://localhost:3000/api/category?category=Fridges");
+            const res = await fetch("/api/category?category=Fridges");
             const data = await res.json();
             const formattedData = Array.isArray(data) ? data : (data.products || []);
             setProducts(formattedData);
